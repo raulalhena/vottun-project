934026939b4dc0348a9b90760703b18fb500564f
Raul <raul.alhena@gmail.com>
2023-11-01 23:39:11 +0100

    created model Users in server

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.DATABASE_URL),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
